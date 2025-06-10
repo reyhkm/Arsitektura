a78311d9dc3301f4d9990a451a99c092a105883b
reyhkm <reyhikam04@gmail.com>
2025-06-10 22:49:24 +0700

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'standalone', // Add this line
   // Optional: If you plan to use external image providers
   // images: {
   //   domains: ['example.com'],
